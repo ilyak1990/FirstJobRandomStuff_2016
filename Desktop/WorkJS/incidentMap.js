05ec16606d86542f3856ad535d42a4905107a83c
ilyak1990 <41556998+ilyak1990@users.noreply.github.com>
2018-08-15 13:20:07 -0400

    Update incidentMap.js

--- Desktop/WorkJS/incidentMap.js
+++ Desktop/WorkJS/incidentMap.js
@@ -1,11 +1,6 @@
-
-
-
-
 <link rel="stylesheet" href="https://unpkg.com/leaflet@1.0.3/dist/leaflet.css"/>
 <link rel="stylesheet" href="https://unpkg.com/leaflet.markercluster@1.0.4/dist/MarkerCluster.Default.css"/>
 <link rel="stylesheet" href="https://unpkg.com/leaflet.markercluster@1.0.4/dist/MarkerCluster.css"/>
-  
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/leaflet.draw/0.4.2/leaflet.draw.css"/>
 
 
@@ -25,68 +20,31 @@
 
   
   getScriptFromCDN("https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.0.3/leaflet.js", false, function() {
-  
-            getScriptFromCDN("https://s3.amazonaws.com/bcic-appjs/leaflet/aerisjs.js", false, function() {
-                getScriptFromCDN("https://unpkg.com/leaflet.markercluster@1.0.4/dist/leaflet.markercluster.js", false, function() {
-                  
-                  getScriptFromCDN("https://cdnjs.cloudflare.com/ajax/libs/leaflet.draw/0.4.9/leaflet.draw.js", false, function() {
+    getScriptFromCDN("https://s3.amazonaws.com/bcic-appjs/leaflet/aerisjs.js", false, function() {
+        getScriptFromCDN("https://unpkg.com/leaflet.markercluster@1.0.4/dist/leaflet.markercluster.js", false, function() {
+           getScriptFromCDN("https://cdnjs.cloudflare.com/ajax/libs/leaflet.draw/0.4.9/leaflet.draw.js", false, function() {
                     
-         
-
-            
-                  
-
-                  
-                  
-                              
-                             
-                                  
-                                  
 mapit();
-                            
-        
-      
-
-  }); 
-    }); 
-
-
+}); 
+}); 
 });
-
-  });
-      
- });
+});
+});
 
 
 function mapit() {
 
-
-  
-  var json=[];
-  
-  
+var json=[];
 
 rbf_selectQuery("SELECT id, Marker_Color,Marker_Icon FROM incident", 1000,  function(values) {
     
   var j = [];
   
   for (var l = 0; l < values.length; l++) { 
-  
-    
-    j[values[l][0]] = {'color':values[l][1], 'icon':values[l][2], 'id':values[l][0]};
-  
-   
-
+j[values[l][0]] = {'color':values[l][1], 'icon':values[l][2], 'id':values[l][0]}  
 }
-
-  json.push(j)
-    
-    
+  json.push(j)   
   });
-  
-
- 
-
 
   $(".loading").hide();
 
@@ -143,16 +101,8 @@ rbf_selectQuery("SELECT id, Marker_Color,Marker_Icon FROM incident", 1000,  func
     }).setView([51.505, -0.09], 2);
   }
   
- 
-  
-  
-       
 
-
-  
-  
-  
-  var types= [];
+var types= [];
 var incidents=[];
 
    types.push("Other")
@@ -167,12 +117,10 @@ incidents.push({'incidentId':{!id},'incident':"{!name#link}" , 'name':"{!name}" 
 
  
 
-      var severe = L.layerGroup([thunderstorm, hurricane, flashflood, tornado]);
-  
- var editableLayers = new L.FeatureGroup();
-    var iconLayers = new L.FeatureGroup();
-
-    var weather = {"Temperature": temperature,
+  var severe = L.layerGroup([thunderstorm, hurricane, flashflood, tornado]);
+  var editableLayers = new L.FeatureGroup();
+  var iconLayers = new L.FeatureGroup();
+  var weather = {"Temperature": temperature,
         "Precipitation": percipitation,
         "Weather Watch": severe ,
         "Hurricane": hurricane,
@@ -202,126 +150,52 @@ incidents.push({'incidentId':{!id},'incident':"{!name#link}" , 'name':"{!name}" 
 
                         if(JSON.parse(values[i][0]).geometry.type=="Point")
                         {
-
-
                             var latlng =  JSON.parse(values[i][0]).geometry.coordinates
-
-
-
-
                             var radius = parseInt(values[i][2]);
-
-
-
-
-                            //var long =  JSON.parse(values[i][1]).geometry.coordinates[0]
-
                             L.circle([latlng[1], latlng[0]], radius).addTo(editableLayers)
-
-
-
-
-
                         }
 
                         else
                         {
-                            console.log(values[i][3])
-
-
+    
                             var ob = JSON.parse(values[i][0]);
 
-
-
-
                             L.geoJSON(ob).addTo(editableLayers);
-
-
-
-
-
-
-
-
 
                         }
                     }
                     else
-                    {
-
-
-                        var icon = L.divIcon({
+                    {     var icon = L.divIcon({
 
                             html:values[i][0],
                             popupAnchor: [0,-10]
-
-
                         })
 
 
                         var customPopup = document.createElement('div');
-
-                        //customPopup.innerHTML = '<i class="fa fa-trash-o trashDelete" style="font-size:large" id="'+ values[i][1] + '" aria-hidden="true"></i>';
-                        console.log(values[i][1])
-                      
-
-
-                       // var customOptions =
-                           // {
-                                //'text-align': 'center',
-
-                               // 'max-width':'100px',
-
-                          //  }
-
-
                         var split = values[i][4].split(',')
 
 
                         var marker= L.marker([split[0] , split[1]], {icon: icon , draggable: false })
-                        // $(marker._icon).addClass(" anIcon")
-                       // marker.bindPopup(customPopup,customOptions)
+  
 
                         marker.addTo(iconLayers)
 
 
 
-                    }
-
-
-
-                }
-
-
-            }
-
-        })
-
-    }
+		    }}}})}
     testy();
 
-
-  
-
-
-  
  var landmarks = {}
- 
  var markerLayers={}
  
  
  
  for (var o = 0; o < types.length; o++) {
    
-   
- 
- 
- 
- var x = landmarks[types[o].replace(/ /g,'') +"M"] = L.layerGroup([])
- 
 
+ var x = landmarks[types[o].replace(/ /g,'') +"M"] = L.layerGroup([])
  markerLayers[types[o]]=x;
-      
 
  }
  console.log("add to controls")
@@ -329,54 +203,21 @@ incidents.push({'incidentId':{!id},'incident':"{!name#link}" , 'name':"{!name}" 
   
   L.control.layers( weather,markerLayers).addTo(map);
   
-  
-
-  
-
-
-  
-
-      
-      
-     
-         
-      
 
 function createMarkers(incidents)
 {
 
-
- 
-
-
-   var cluster = {}
- 
- var clusterLayers={}
- 
-
-  var clustergroup = L.markerClusterGroup();
+var cluster = {}
+var clusterLayers={}
+var clustergroup = L.markerClusterGroup();
   
   
   for (var i = 0; i < incidents.length; i++) { 
-    
-   
-
-  
-    
-    
-
 
      for (var t = 0; t < incidents[i].location_data.length; t++) { 
        
-       
-  
-       
-    
         var link ='{!#LINK.org_location#113406}';
-        
-        link+= "&id=" + 1 ;
-
-    
+        link+= "&id=" + 1 ;    
         var title = "<div><a class='locations' href='"+ link  +"'>"    + " " + incidents[i].incident + "</a><span id='deleteButton' onclick='test(" + incidents[i].incidentId + ")'></span><br>" + incidents[i].location_data[t].location  + "<br>"+ incidents[i].status  + "</div>";
  
       
@@ -395,29 +236,18 @@ function createMarkers(incidents)
    if(incidents[i].incidentId !== null )
         {
      
-             if(incidents[i].icon !== '') var fa = '<i class=' + '"' +  incidents[i].icon + '"'  + '></i>';
-               
-          else var fa='';
+             if(incidents[i].icon !== '') var fa = '<i class=' + '"' +  incidents[i].icon + '"'  + '></i>';    
+             else var fa='';
 
            
-          if(incidents[i].color !== '') var color = incidents[i].color;
-          
-   
-          else var color = 0;
-          
-          
-     
+             if(incidents[i].color !== '') var color = incidents[i].color;
+             else var color = 0;
+
         }
-     
-
-            
-      
-
 
 if(fa !== '') var svgrect = '<div id="svg"><svg width="32px" height="52px" viewBox="0 0 32 52" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><path d="M16,1 C7.7146,1 1,7.65636364 1,15.8648485 C1,24.0760606 16,51 16,51 C16,51 31,24.0760606 31,15.8648485 C31,7.65636364 24.2815,1 16,1 L16,1 Z" fill="'+ color +'"></path>'+ fa + '</svg></div>'
 
 else var svgrect = '<div id="svg"><svg width="32px" height="52px" viewBox="0 0 32 52" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><path d="M16,1 C7.7146,1 1,7.65636364 1,15.8648485 C1,24.0760606 16,51 16,51 C16,51 31,24.0760606 31,15.8648485 C31,7.65636364 24.2815,1 16,1 L16,1 Z" fill="'+ color +'"></path><circle class="svg-icon-circle" cx="16" cy="16" r="8" fill="white"  stroke="black" stroke-width="1.5" stroke-opacity="0.5"></circle></svg></div>'
-
 
 
 var icon =  L.divIcon({
@@ -427,9 +257,6 @@ var icon =  L.divIcon({
      html: svgrect
   
        });
-      
-
-
 
         var marker= L.marker([incidents[i].location_data[t].lat, incidents[i].location_data[t].long], {id: incidents[i].incidentId, icon: icon , draggable: false }).addTo(landmarks[theClass])
         
@@ -450,87 +277,32 @@ var icon =  L.divIcon({
       console.log('marker dragend event');
      
     });
-   
-      
-
-      
-
-     
-      
-    
-    
-    
-     
-  
-                      }
-  }
+                      }};
   
   
   clustergroup.on('clusterclick', function (a) {
-	
-
-   
-
-    
-    
     setTimeout(function(){  applyCSS();}, 100);
-    
-   
 });
-  
-  
 
-
-  
-  
  map.addLayer(clustergroup);
 
-
-
-    
-  
-     clustergroup.on('unspiderfied', function (a) {
+ clustergroup.on('unspiderfied', function (a) {
      
      for (var i = 0; i <  $('.leaflet-control-layers-overlays').find('div').find('input:checked').siblings().length; i++) {
-       
-       
-      
-     
+
        var text = $('.leaflet-control-layers-overlays').find('div').find('input:checked').siblings()[i].innerHTML
-       
-  
-      var final= text.replace(/ /g,'') +"M"
-       
-       
-      
-      
+       var final= text.replace(/ /g,'') +"M"
+
        map.removeLayer(landmarks[final])
-      
        map.addLayer(landmarks[final])
-       
-       
-     
-          
-       
      }
-    
-
-     
-     
-   
 });
-  
- 
+
     map.on( "zoomend", function( ) {
-         
- 
- 
+
  applyCSS();
-
-
   });
-  
-  
+
   iconLayers.on( "add", function(e ) {
 
             for (var i in e.target._layers) {
@@ -544,65 +316,26 @@ var icon =  L.divIcon({
 $('div.leaflet-marker-icon.anIcon').find('i').css('color','black')
     $('div.leaflet-marker-icon.anIcon').find('i').css('font-size','xx-large')
     $('div.leaflet-marker-icon.anIcon').css('border','none')
-    $('div.leaflet-marker-icon.anIcon').css('background','none')
+    $('div.leaflet-marker-icon.anIcon').css('background','none')});
 
-
-        });
-
-    
-    
-    
-map.on('overlayadd', function(e) {
- 
-  
-applyCSS();
-
-    
-  });
-
-    
-
-    
-    
-
+	map.on('overlayadd', function(e) {applyCSS();});
                       }
                       
  createMarkers(incidents);
 
-
-
-
     $('input.leaflet-marker-shadow').css('visibility' , 'hidden'); //hides shadows behind the markers
-
-  applyCSS();
-
-
-
- 
-
-
-
-
-}
+applyCSS();}
 
 
 
 
 function applyCSS(){
-  
-  console.log("applying")
-  
 
-   $('div.leaflet-marker-icon').css('height',0);
+$('div.leaflet-marker-icon').css('height',0);
 $('div.leaflet-marker-icon').css('border','none');
-    $('div.leaflet-marker-icon').css('margin-left','-50.5px');
-
-  $('#svg svg').css('display','block')
+$('div.leaflet-marker-icon').css('margin-left','-50.5px');
+$('#svg svg').css('display','block')
 $('#svg svg').css('width','100px')
-
-
-
-
 $('.leaflet-marker-icon').find('i.fa').css('z-index','2')
 $('.leaflet-marker-icon').find('i.fa').css('width','100px')
 $('.leaflet-marker-icon').find('i.fa').css('text-align','center')
@@ -610,12 +343,10 @@ $('.leaflet-marker-icon').find('i.fa').css('display','block')
 $('.leaflet-marker-icon').find('i.fa').css('top','10px')
 $('.leaflet-marker-icon').find('i.fa').css('position','absolute')
 $('.leaflet-marker-icon').find('i.fa').css('color','white')
-
 $('div.leaflet-marker-icon.anIcon').find('i').css('color','black')
-    $('div.leaflet-marker-icon.anIcon').find('i').css('font-size','xx-large')
-    $('div.leaflet-marker-icon.anIcon').css('border','none')
-    $('div.leaflet-marker-icon.anIcon').css('background','none')
-
+$('div.leaflet-marker-icon.anIcon').find('i').css('font-size','xx-large')
+$('div.leaflet-marker-icon.anIcon').css('border','none')
+$('div.leaflet-marker-icon.anIcon').css('background','none')
 }
 
 
@@ -627,22 +358,15 @@ function onSubmit(e)
 
 function test(e)
 {
-  
             kendo.confirm("Are you sure that you want to delete?").then(function () {
-                kendo.alert("This location has been deleted.");
-              
-              rbf_deleteRecord('incident', e, function(values){})
-              
-              window.location.reload();
+            kendo.alert("This location has been deleted.");
+            rbf_deleteRecord('incident', e, function(values){})
+            window.location.reload();
             } 
                );
-        
-
 }
 
 function onClick(e) {}
-
-
 function getScriptFromCDN(url, isCache, callback)
 {
     jQuery.ajax({
@@ -653,11 +377,6 @@ function getScriptFromCDN(url, isCache, callback)
             cache: isCache
     });
 };
-
-
-
-
-
 
 </script>
 
