e766773fdcd25cb08427f19a09eeadf356354a2a
ilyak1990 <41556998+ilyak1990@users.noreply.github.com>
2018-08-15 13:08:18 -0400

    Update LambdaParams.js

--- Desktop/AWS/LambdaParams.js
+++ Desktop/AWS/LambdaParams.js
@@ -2,16 +2,10 @@ var filename = '{!file}';
 var fileext = filename.split('.').pop();
 var fileext = fileext.toLowerCase();
 
-
-rbv_api.println(fileext)
-
 var url="{!file#url}"
-
-rbv_api.println(url)
 
 
 var headers = {"x-api-key":"xxxAPIKEY","custId":{!#CURR_CUSTM.id},"attachID":{!id},"fileURL": url, "fileType":fileext, "fileName":"{!name#text}"+"."+fileext}
-
 
 var response = rbv_api.sendJSONRequest('https://xx.execute-api.us-east-1.amazonaws.com/beta/loginAPI', null, 'POST', 'application/json', null, null, headers,null)
 
